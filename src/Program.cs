8f53ca5d8ee85e106946cae8bbf3cb743a9171f7
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-04-21 16:39:20 +0200

    Add project base

---
+++ src/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Splaak
+{
+    public class Program
+    {
+        /// <summary>
+        /// Opens the REPL of Splaak.
+        /// </summary>
+        /// <param name="args">The arguments of running the application.</param>
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}

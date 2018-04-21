d963ee5a87529e6b5cb7548549fe28678f37c6f7
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-04-21 18:33:20 +0200

    Add simple, quite broken, reader

--- src/Core/Reader/Reader.cs
+++ src/Core/Reader/Reader.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using Splaak.Core.Reader.Expressions;
 
 namespace Splaak.Core.Reader
 {
+    //TODO: FIX THIS MESS.
+
     /// <summary>
     /// Class used for reading Splaak code.
     /// </summary>
@@ -15,12 +19,73 @@ namespace Splaak.Core.Reader
         /// <returns>S-expression presented in the input.</returns>
         public static ISExpression Read(string input)
         {
-            return Read(input, 0);
+            return Read(new StringReader(input));
         }
 
-        private static ISExpression Read(string input, int position)
+        private static ISExpression Read(StringReader reader)
         {
-            throw new NotImplementedException();
+            while (true)
+            {
+                char c = (char) reader.Peek();
+                if (c == '(')
+                {
+                    reader.Read();
+                    return ReadList(reader);
+                }
+                else if (c == ')')
+                {
+                    throw new ReaderException();
+                }
+                else if (c >= 33 && c <= 126)
+                {
+                    return ReadWord(reader);
+                }
+                else
+                {
+                    reader.Read();
+                }
+            }
+        }
+
+        private static ISExpression ReadList(StringReader reader)
+        {
+            List<ISExpression> expressions = new List<ISExpression>();
+
+            while (true)
+            {
+                char c = (char) reader.Peek();
+                if (c == ')')
+                {
+                    reader.Read();
+                    return new SList(expressions.ToArray());
+                }
+                else if (c >= 33 && c <= 126)
+                {
+                    expressions.Add(Read(reader));
+                }
+                else
+                {
+                    reader.Read();
+                }
+            }
+        }
+
+        private static ISExpression ReadWord(StringReader reader)
+        {
+            StringBuilder sb = new StringBuilder();
+            while (true)
+            {
+                char c = (char) reader.Peek();
+                if (c >= 33 && c <= 126 && c != '(' && c != ')')
+                {
+                    reader.Read();
+                    sb.Append(c);
+                }
+                else
+                {
+                    return new SSym(sb.ToString());
+                }
+            }
         }
     }
 }
---
+++ src/Core/Reader/ReaderException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Splaak.Core.Reader
+{
+    /// <summary>
+    /// Exception for when reading the program goes wrong.
+    /// </summary>
+    /// <seealso cref="System.Exception" />
+    public class ReaderException : Exception { }
+}

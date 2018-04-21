563145321bcf74eba203c2f0da2f8b2ba03d71cc
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-04-21 17:05:02 +0200

    Add SList class

---
+++ src/Core/Reader/Expressions/SList.cs
@@ -0,0 +1,36 @@
+﻿using Splaak.Core.AbstractSyntax;
+
+namespace Splaak.Core.Reader.Expressions
+{
+    /// <summary>
+    /// Represents a list of S-expressions.
+    /// </summary>
+    /// <seealso cref="Splaak.Core.Reader.Expressions.ISExpression" />
+    public class SList : ISExpression
+    {
+        /// <summary>
+        /// The s-expressions contained in this list.
+        /// </summary>
+        public readonly ISExpression[] Expressions;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SList"/> class.
+        /// </summary>
+        /// <param name="expressions">The expressions contained in this list.</param>
+        public SList(ISExpression[] expressions)
+        {
+            Expressions = expressions;
+        }
+
+        /// <summary>
+        /// Parses this s-expression.
+        /// </summary>
+        /// <returns>
+        /// Abstract syntax-tree structure.
+        /// </returns>
+        public IExprExt Parse()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

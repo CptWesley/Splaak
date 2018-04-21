8f29f8f537b9f14be5cfd782d61cb454fce07927
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-04-21 19:24:41 +0200

    Add Value subclasses

---
+++ src/Core/Values/BoolV.cs
@@ -0,0 +1,31 @@
+﻿namespace Splaak.Core.Values
+{
+    /// <summary>
+    /// Represents a boolean.
+    /// </summary>
+    /// <seealso cref="Splaak.Core.Values.IValue" />
+    public class BoolV : IValue
+    {
+        /// <summary>
+        /// The boolean value.
+        /// </summary>
+        public readonly bool Value;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IntV"/> class.
+        /// </summary>
+        /// <param name="value">The boolean value.</param>
+        public BoolV(bool value)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString() => string.Format("BoolV({0})", Value);
+    }
+}

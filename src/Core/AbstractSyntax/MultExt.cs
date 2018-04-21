d7e8ee3c1a42a3cd60efb17a151ad6880c0fd73e
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-04-21 19:17:32 +0200

    Add MultExt class

---
+++ src/Core/AbstractSyntax/MultExt.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Splaak.Core.CoreSyntax;
+
+namespace Splaak.Core.AbstractSyntax
+{
+    /// <summary>
+    /// Represents a multiplication.
+    /// </summary>
+    /// <seealso cref="Splaak.Core.AbstractSyntax.IExprExt" />
+    public class MultExt : IExprExt
+    {
+        /// <summary>
+        /// The arguments for the multiplication.
+        /// </summary>
+        public readonly IExprExt Argument1, Argument2;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlusExt"/> class.
+        /// </summary>
+        /// <param name="arg1">The left argument of the multiplication.</param>
+        /// <param name="arg2">The right argument of the multiplication.</param>
+        public MultExt(IExprExt arg1, IExprExt arg2)
+        {
+            Argument1 = arg1;
+            Argument2 = arg2;
+        }
+
+        /// <summary>
+        /// Desugars this abstract expression.
+        /// </summary>
+        /// <returns>
+        /// Core expression variant.
+        /// </returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public IExprC Desugar()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString() => string.Format("MultExt({0}, {1})", Argument1.ToString(), Argument2.ToString());
+    }
+}

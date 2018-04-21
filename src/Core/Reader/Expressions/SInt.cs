acd7c83423c6a2801fa110c396fa86c1fc85d0b9
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-04-21 17:09:05 +0200

    Add SInt class

---
+++ src/Core/Reader/Expressions/SInt.cs
@@ -0,0 +1,37 @@
+﻿using Splaak.Core.AbstractSyntax;
+
+namespace Splaak.Core.Reader.Expressions
+{
+    /// <summary>
+    /// Represents an integer.
+    /// </summary>
+    /// <seealso cref="Splaak.Core.Reader.Expressions.ISExpression" />
+    public class SInt : ISExpression
+    {
+        /// <summary>
+        /// The value of the expression.
+        /// </summary>
+        public readonly int Value;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SInt"/> class.
+        /// </summary>
+        /// <param name="value">The value of the expression.</param>
+        public SInt(int value)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        /// Parses this s-expression.
+        /// </summary>
+        /// <returns>
+        /// Abstract syntax-tree structure.
+        /// </returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public IExprExt Parse()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

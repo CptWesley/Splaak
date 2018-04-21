d0a58ca8bded26381644f405a896ba973d0c9922
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-04-21 17:21:22 +0200

    Add Reader class

---
+++ src/Core/Reader/Reader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Splaak.Core.Reader.Expressions;
+
+namespace Splaak.Core.Reader
+{
+    /// <summary>
+    /// Class used for reading Splaak code.
+    /// </summary>
+    public static class Reader
+    {
+        /// <summary>
+        /// Reads the specified input.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <returns>S-expression presented in the input.</returns>
+        public static ISExpression Read(string input)
+        {
+            return Read(input, 0);
+        }
+
+        private static ISExpression Read(string input, int position)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

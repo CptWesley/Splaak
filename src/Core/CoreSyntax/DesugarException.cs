e0dbb3c20249fb1d717d511d141b4d1e9ec0f6a0
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-04-21 19:33:57 +0200

    Add exceptions

---
+++ src/Core/CoreSyntax/DesugarException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Splaak.Core.CoreSyntax
+{
+    /// <summary>
+    /// Exception for when desugaring the program goes wrong.
+    /// </summary>
+    /// <seealso cref="System.Exception" />
+    public class DesugarException : Exception { }
+}
---
+++ src/Core/Values/InterpretException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Splaak.Core.Values
+{
+    /// <summary>
+    /// Exception for when interpreting the program goes wrong.
+    /// </summary>
+    /// <seealso cref="System.Exception" />
+    public class InterpretException : Exception { }
+}

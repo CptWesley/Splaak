3f1ae0b29aada4d70d2ec9e2d0953eed5f02cd59
Paul <pvdstel@users.noreply.github.com>
2018-04-22 14:58:31 +0200

    Add more tests for SReader

--- tests/src/Reader/SReaderTests.cs
+++ tests/src/Reader/SReaderTests.cs
@@ -48,12 +48,34 @@ namespace Splaak.Tests.Reader
             Assert.Equal(SReader.Read("(   5    true  )"), new SList(new ISExpression[] { new SInt(5), new SSym("true") }));
         }
 
-        /*
         [Fact]
-        public void BracketMismatchTest()
+        public void ListMultipleExtraExtraSpacesTest()
+        {
+            Assert.Equal(SReader.Read("    (  if true  5    2  )    "), new SList(new ISExpression[] { new SSym("if"), new SSym("true"), new SInt(5), new SInt(2) }));
+        }
+
+        [Fact]
+        public void BracketMismatchOpenTest()
         {
             Assert.Throws<ReaderException>(() => SReader.Read("("));
         }
-        */
+
+        [Fact]
+        public void BracketMismatchCloseTest()
+        {
+            Assert.Throws<ReaderException>(() => SReader.Read(")"));
+        }
+
+        [Fact]
+        public void BracketMismatchContentOpenTest()
+        {
+            Assert.Throws<ReaderException>(() => SReader.Read("(if true 5 0"));
+        }
+
+        [Fact]
+        public void BracketMismatchContentCloseTest()
+        {
+            Assert.Throws<ReaderException>(() => SReader.Read("22    )"));
+        }
     }
 }

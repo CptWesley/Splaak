ea29b2efc916d9b457896baf1fb8369b1d6d2b1e
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-04-22 08:29:26 +0200

    Add Values equalities and hashcodes

--- core/src/Values/BoolV.cs
+++ core/src/Values/BoolV.cs
@@ -27,5 +27,32 @@
         /// A <see cref="System.String" /> that represents this instance.
         /// </returns>
         public override string ToString() => string.Format("BoolV({0})", Value);
+
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" />, is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is BoolV)
+            {
+                return ((BoolV) obj).Value == Value;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return GetType().GetHashCode() * Value.GetHashCode();
+        }
     }
 }
--- core/src/Values/FloatV.cs
+++ core/src/Values/FloatV.cs
@@ -1,4 +1,6 @@
-﻿namespace Splaak.Core.Values
+﻿using System;
+
+namespace Splaak.Core.Values
 {
     /// <summary>
     /// Represents a float.
@@ -27,5 +29,32 @@
         /// A <see cref="System.String" /> that represents this instance.
         /// </returns>
         public override string ToString() => string.Format("FloatV({0})", Value);
+
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" />, is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is FloatV)
+            {
+                return Math.Abs(((FloatV) obj).Value - Value) < 0.001;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return GetType().GetHashCode() * Value.GetHashCode();
+        }
     }
 }

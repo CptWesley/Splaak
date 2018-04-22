cd64b48b91d301d3c3c9ae44f2d33e81fe2ca95b
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-04-22 19:32:25 +0200

    Add simple repl

---
+++ splaak_repl/src/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Splaak.Core;
+using Splaak.Core.Values;
+
+namespace Splaak.Repl
+{
+    /// <summary>
+    /// REPL for the Splaak language.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Entry method of the program
+        /// </summary>
+        /// <param name="args">Program arguments.</param>
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("splaak> ");
+                string input = Console.ReadLine();
+                try
+                {
+                    IValue result = SInterpreter.Interpret(input);
+                    Console.WriteLine(result);
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(e);
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

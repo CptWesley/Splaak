35a1b241f0c36543a2d30b88b6d32ba121eeff28
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-04-22 10:21:33 +0200

    Add abstract syntax tests

---
+++ tests/src/AbstractSyntax/IntExtTests.cs
@@ -0,0 +1,60 @@
+﻿using Splaak.Core.AbstractSyntax;
+using Splaak.Core.CoreSyntax;
+using Xunit;
+
+namespace Splaak.Tests.AbstractSyntax
+{
+    public class IntExtTests
+    {
+        private const int Value = 128;
+        private IntExt _obj = new IntExt(Value);
+
+        [Fact]
+        public void ConstructorTest()
+        {
+            Assert.Equal(Value, _obj.Value);
+        }
+
+        [Fact]
+        public void EqualsEqualTest()
+        {
+            Assert.True(_obj.Equals(new IntExt(Value)));
+        }
+
+        [Fact]
+        public void EqualsNullTest()
+        {
+            Assert.False(_obj.Equals(null));
+        }
+
+        [Fact]
+        public void EqualsNotEqualValueTest()
+        {
+            Assert.False(_obj.Equals(new IntExt(Value + 1)));
+        }
+
+        [Fact]
+        public void EqualsNotEqualTypeTest()
+        {
+            Assert.False(_obj.Equals(new BoolExt(false)));
+        }
+
+        [Fact]
+        public void HashCodeEqualTest()
+        {
+            Assert.Equal(_obj.GetHashCode(), new IntExt(Value).GetHashCode());
+        }
+
+        [Fact]
+        public void ToStringTest()
+        {
+            Assert.Equal(_obj.ToString(), "IntExt(" + Value + ")");
+        }
+
+        [Fact]
+        public void DesugarTest()
+        {
+            Assert.Equal(_obj.Desugar(), new IntC(Value));
+        }
+    }
+}
---
+++ tests/src/AbstractSyntax/NotExtTests.cs
@@ -0,0 +1,60 @@
+﻿using Splaak.Core.AbstractSyntax;
+using Splaak.Core.CoreSyntax;
+using Xunit;
+
+namespace Splaak.Tests.AbstractSyntax
+{
+    public class NotExtTests
+    {
+        private static IExprExt Value = new BoolExt(true);
+        private NotExt _obj = new NotExt(Value);
+
+        [Fact]
+        public void ConstructorTest()
+        {
+            Assert.Equal(Value, _obj.Argument);
+        }
+
+        [Fact]
+        public void EqualsEqualTest()
+        {
+            Assert.True(_obj.Equals(new NotExt(Value)));
+        }
+
+        [Fact]
+        public void EqualsNullTest()
+        {
+            Assert.False(_obj.Equals(null));
+        }
+
+        [Fact]
+        public void EqualsNotEqualValueTest()
+        {
+            Assert.False(_obj.Equals(new NotExt(new IntExt(0))));
+        }
+
+        [Fact]
+        public void EqualsNotEqualTypeTest()
+        {
+            Assert.False(_obj.Equals(new IntExt(0)));
+        }
+
+        [Fact]
+        public void HashCodeEqualTest()
+        {
+            Assert.Equal(_obj.GetHashCode(), new NotExt(Value).GetHashCode());
+        }
+
+        [Fact]
+        public void ToStringTest()
+        {
+            Assert.Equal(_obj.ToString(), "NotExt(" + Value + ")");
+        }
+
+        [Fact]
+        public void DesugarTest()
+        {
+            Assert.Equal(_obj.Desugar(), new IfC(Value.Desugar(), new BoolC(false), new BoolC(true)));
+        }
+    }
+}

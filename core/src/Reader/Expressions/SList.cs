561a83ac8197c3014554a1a315d5a6ba9301cc29
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-04-22 08:29:08 +0200

    Add s-expression equalities and hashcodes

--- core/src/Reader/Expressions/SList.cs
+++ core/src/Reader/Expressions/SList.cs
@@ -84,5 +84,51 @@ namespace Splaak.Core.Reader.Expressions
             sb.Append(")");
             return sb.ToString();
         }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" />, is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is SList)
+            {
+                SList that = (SList) obj;
+                if (that.Expressions.Length != Expressions.Length)
+                {
+                    return false;
+                }
+                for (int i = 0; i < Expressions.Length; ++i)
+                {
+                    if (!that.Expressions[i].Equals(Expressions[i]))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
+        public override int GetHashCode()
+        {
+            int result = GetType().GetHashCode();
+
+            for (int i = 0; i < Expressions.Length; ++i)
+            {
+                result *= Expressions[i].GetHashCode() ^ (i + 1);
+            }
+
+            return result;
+        }
     }
 }

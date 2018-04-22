4726ac196e1da8191c00bbf3c824b6c08096faa9
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-04-22 10:04:05 +0200

    Add core syntax tests

---
+++ tests/src/CoreSyntax/PlusCTests.cs
@@ -0,0 +1,92 @@
+﻿using Splaak.Core.CoreSyntax;
+using Splaak.Core.Values;
+using Xunit;
+
+namespace Splaak.Tests.CoreSyntax
+{
+    public class PlusCTests
+    {
+        private static IntC _1 = new IntC(10);
+        private static IntC _2 = new IntC(2);
+        private PlusC _obj = new PlusC(_1, _2);
+
+        [Fact]
+        public void ConstructorTest()
+        {
+            Assert.Equal(_obj.Argument1, _1);
+            Assert.Equal(_obj.Argument2, _2);
+        }
+
+        [Fact]
+        public void EqualsEqualTest()
+        {
+            Assert.True(_obj.Equals(new PlusC(_1, _2)));
+        }
+
+        [Fact]
+        public void EqualsNullTest()
+        {
+            Assert.False(_obj.Equals(null));
+        }
+
+        [Fact]
+        public void EqualsNotEqualValueTest()
+        {
+            Assert.False(_obj.Equals(new PlusC(_1, _1)));
+        }
+
+        [Fact]
+        public void EqualsNotEqualTypeTest()
+        {
+            Assert.False(_obj.Equals(new BoolC(false)));
+        }
+
+        [Fact]
+        public void HashCodeEqualTest()
+        {
+            Assert.Equal(_obj.GetHashCode(), new PlusC(_1, _2).GetHashCode());
+        }
+
+        [Fact]
+        public void ToStringTest()
+        {
+            Assert.Equal(_obj.ToString(), "PlusC(" + _1 + ", " + _2 + ")");
+        }
+
+        [Fact]
+        public void InterpretIntIntTest()
+        {
+            Assert.Equal(new PlusC(new IntC(10), new IntC(3)).Interpret(), new IntV(13));
+        }
+
+        [Fact]
+        public void InterpretFloatFloatTest()
+        {
+            Assert.Equal(new PlusC(new FloatC(10), new FloatC(5)).Interpret(), new FloatV(15));
+        }
+
+        [Fact]
+        public void InterpretFloatIntTest()
+        {
+            Assert.Equal(new PlusC(new FloatC(10), new IntC(5)).Interpret(), new FloatV(15));
+        }
+
+        [Fact]
+        public void InterpretIntFloatTest()
+        {
+            Assert.Equal(new PlusC(new IntC(10), new FloatC(5)).Interpret(), new FloatV(15));
+        }
+
+        [Fact]
+        public void InterpretLeftNaNExceptionTest()
+        {
+            Assert.Throws<InterpretException>(() => new PlusC(new BoolC(true), new FloatC(5)).Interpret());
+        }
+
+        [Fact]
+        public void InterpretRightNaNExceptionTest()
+        {
+            Assert.Throws<InterpretException>(() => new PlusC(new FloatC(5), new BoolC(true)).Interpret());
+        }
+    }
+}

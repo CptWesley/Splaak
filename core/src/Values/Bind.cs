6a4f9a02f9eb6306eb81bcd171cccddd4f2d8ebd
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-04-23 18:06:02 +0200

    Add Bind class and tests

---
+++ core/src/Values/Bind.cs
@@ -0,0 +1,64 @@
+﻿namespace Splaak.Core.Values
+{
+    /// <summary>
+    /// Represents an environment bind.
+    /// </summary>
+    public class Bind
+    {
+        /// <summary>
+        /// The identifier of this bind.
+        /// </summary>
+        public readonly string Id;
+
+        /// <summary>
+        /// The value of this bind.
+        /// </summary>
+        public IValue Value { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Bind"/> class.
+        /// </summary>
+        /// <param name="id">The identifier of the bind.</param>
+        /// <param name="value">The value of the bind.</param>
+        public Bind(string id, IValue value)
+        {
+            Id = id;
+            Value = value;
+        }
+
+        /// <summary>
+        /// Returns a <see cref="string" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string" /> that represents this instance.
+        /// </returns>
+        public override string ToString() => $"Bind({Id}, {Value})";
+
+        /// <summary>
+        /// Determines whether the specified <see cref="object" />, is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="object" /> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="object" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is Bind that)
+            {
+                return that.Id.Equals(Id) && that.Value.Equals(Value);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return GetType().GetHashCode() * Id.GetHashCode() * Value.GetHashCode() ^ 2;
+        }
+    }
+}
---
+++ tests/src/Values/BindTests.cs
@@ -0,0 +1,61 @@
+﻿using Splaak.Core.Values;
+using Xunit;
+
+namespace Splaak.Tests.Values
+{
+    public class BindTests
+    {
+        private static string _1 = "greatname";
+        private static IValue _2 = new IntV(1);
+        private Bind _obj = new Bind(_1, _2);
+
+        [Fact]
+        public void ConstructorTest()
+        {
+            Assert.Equal(_obj.Id, _1);
+            Assert.Equal(_obj.Value, _2);
+        }
+
+        [Fact]
+        public void EqualsEqualTest()
+        {
+            Assert.True(_obj.Equals(new Bind(_1, _2)));
+        }
+
+        [Fact]
+        public void EqualsNullTest()
+        {
+            Assert.False(_obj.Equals(null));
+        }
+
+        [Fact]
+        public void EqualsNotEqualIdTest()
+        {
+            Assert.False(_obj.Equals(new Bind("", _2)));
+        }
+
+        [Fact]
+        public void EqualsNotEqualValueTest()
+        {
+            Assert.False(_obj.Equals(new Bind(_1, new IntV(0))));
+        }
+
+        [Fact]
+        public void EqualsNotEqualTypeTest()
+        {
+            Assert.False(_obj.Equals(new IntV(0)));
+        }
+
+        [Fact]
+        public void HashCodeEqualTest()
+        {
+            Assert.Equal(_obj.GetHashCode(), new Bind(_1, _2).GetHashCode());
+        }
+
+        [Fact]
+        public void ToStringTest()
+        {
+            Assert.Equal(_obj.ToString(), $"Bind({_1}, {_2})");
+        }
+    }
+}

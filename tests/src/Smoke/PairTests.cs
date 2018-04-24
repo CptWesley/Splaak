9552b8eaea70ec7250ab0f8e5e507c339db8b766
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-04-24 06:46:49 +0200

    Change SInterpreter to no longer always force the result to prevent infinite loops

--- tests/src/Smoke/PairTests.cs
+++ tests/src/Smoke/PairTests.cs
@@ -9,31 +9,31 @@ namespace Splaak.Tests.Smoke
         [Fact]
         public void PairTest()
         {
-            Assert.Equal(SInterpreter.Interpret("(pair 3 4)"), new PairV(new IntV(3), new IntV(4)));
+            Assert.Equal(SInterpreter.Interpret("(pair 3 4)").Force(), new PairV(new IntV(3), new IntV(4)));
         }
 
         [Fact]
         public void FirstTest()
         {
-            Assert.Equal(SInterpreter.Interpret("(first (pair 3 4))"), new IntV(3));
+            Assert.Equal(SInterpreter.Interpret("(first (pair 3 4))").Force(), new IntV(3));
         }
 
         [Fact]
         public void SecondTest()
         {
-            Assert.Equal(SInterpreter.Interpret("(second (pair 3 4))"), new IntV(4));
+            Assert.Equal(SInterpreter.Interpret("(second (pair 3 4))").Force(), new IntV(4));
         }
 
         [Fact]
         public void NestedTest()
         {
-            Assert.Equal(SInterpreter.Interpret("(first (second (second (second (pair 0 (pair 1 (pair 2 (pair 3 4))))))))"), new IntV(3));
+            Assert.Equal(SInterpreter.Interpret("(first (second (second (second (pair 0 (pair 1 (pair 2 (pair 3 4))))))))").Force(), new IntV(3));
         }
 
         [Fact]
         public void TupleTest()
         {
-            Assert.Equal(SInterpreter.Interpret("(tuple 1 2 3 4 5)"),
+            Assert.Equal(SInterpreter.Interpret("(tuple 1 2 3 4 5)").Force(),
                 new PairV(new IntV(1),
                     new PairV(new IntV(2),
                         new PairV(new IntV(3), 
